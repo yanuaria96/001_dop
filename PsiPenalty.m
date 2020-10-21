863cbda856f9010b990b5abbf79da31b06cc944c
Yana <yanuaria96@mail.ru>
2020-10-21 20:23:32 +0700

    Changes at the lesson 19-10-2020.

---
+++ PsiPenalty.m
@@ -0,0 +1,7 @@
+function Penalty = PsiPenalty(x_cur,y_cur)
+    E = 10;
+    penetration_x = max(abs(x_cur - 1) - 1,0);
+    penetration_y = max(abs(y_cur - 1/2) - 1/2,0);
+    penetration2 = penetration_x^2 + penetration_y^2;
+    Penalty = penetration2 * E * 1/2;
+end
--- rect_pdf.m
+++ rect_pdf.m
@@ -2,11 +2,11 @@ function rect_pdf()
 % PDF - probability distribudion function
 t=tic;
 global D_X D_Y D_T ETTA W H W_2 H_2
-N = 80;
-c1_const = 0.0001;
-D_X = c1_const;
+N = 2;
+c1_const = 0.000001;
+D_X = c1_const; %perturbation incriment
 D_Y = c1_const;
-D_T = .04*N^(-2);
+D_T = .01*N^(-2);
 ETTA = 1;
 W = 2; % picture width
 H = 1; % picture height
@@ -19,6 +19,7 @@ vx = zeros(1,N); vy = zeros(1,N);
 handler = CreateRectangle(W, H);
 handler = VisualizePoints(x, y, handler);
 
+sumPsi_old = 1e100;
 for iter = 1:10000
     % Find potentials for each point
     maxVelocity = 0;
@@ -28,6 +29,9 @@ for iter = 1:10000
         y_cur = y(k);
         x_other = [x(1:k-1) x(k+1:end)];
         y_other = [y(1:k-1) y(k+1:end)];
+%         x_other = x(1:k-1);
+%         y_other = y(1:k-1);
+
         
         [vx_cur, vy_cur, sumPsi_cur] = FindVelocity(x_cur, y_cur, x_other, y_other);
         
@@ -39,17 +43,31 @@ for iter = 1:10000
         maxVelocity = max([v2 maxVelocity]);
     end
    
-    if maxVelocity < 3e-4
-        D_T = .04*N^(-2)*(maxVelocity/3e-4)^(-1/2);
-    end
+    if sumPsi>sumPsi_old
+        D_T = D_T/1.1;
+%         x = x_old;
+%         y = y_old;
+    else
+        D_T = D_T*1.1;
+    
+%     if maxVelocity < 3e-4
+%         D_T = .04*N^(-2)*(maxVelocity/3e-4)^(-1/2);
+%     end
     
     if maxVelocity < 3e-10
         break;
     end
     
     % Move each object according to its velocity
-    x = x + vx;
-    y = y + vy;
+    x_old = x;
+    y_old = y;
+    sumPsi_old = sumPsi;
+    
+    x = x + vx*D_T;
+    y = y + vy*D_T;
+
+    end
+    
     
     if rem(iter,1) == 0
         handler = VisualizePoints(x, y, handler);
@@ -87,8 +105,7 @@ function obj = MoveObject(obj)
 end
 
 function [vx, vy, sumPsi] = FindVelocity(x_cur, y_cur, x_other, y_other)
-%global D_THETA D_PHI ETTA D_T 
-global D_X D_Y D_T ETTA
+global D_X D_Y ETTA
 
 N = length(x_other);
 sumPsi = 0;
@@ -99,31 +116,27 @@ for k = 1:N
     oy = y_other(k); % other y
     
     psiUnmoved = PSI(x_cur, y_cur, ox, oy);
-
-    dPsidX = -(D_T/ETTA).*(1/D_X).*(PSI(x_cur+D_X, y_cur, ox, oy) - psiUnmoved);
-    dPsidY = -(D_T/ETTA).*(1/D_Y).*(PSI(x_cur, y_cur+D_Y, ox, oy) - psiUnmoved);
+    
+    dPsidX = -(1/ETTA)*(1/D_X)*(PSI(x_cur+D_X, y_cur, ox, oy) - psiUnmoved);
+    dPsidY = -(1/ETTA)*(1/D_Y)*(PSI(x_cur, y_cur+D_Y, ox, oy) - psiUnmoved);
 
     vx = vx + dPsidX;
     vy = vy + dPsidY;
 
     sumPsi = sumPsi + psiUnmoved;
 end
+PsiPenUnmoved = PsiPenalty(x_cur, y_cur);
+dPsidX = -(1/ETTA)*(1/D_X)*(PsiPenalty(x_cur+D_X, y_cur) - PsiPenUnmoved);
+dPsidY = -(1/ETTA)*(1/D_Y)*(PsiPenalty(x_cur, y_cur+D_Y) - PsiPenUnmoved);
+vx = vx + dPsidX;
+vy = vy + dPsidY;
+sumPsi = sumPsi + PsiPenUnmoved;
 end
 
 function psi = PSI(x1, y1, x2, y2)
 global W_2 H_2
 % input: (x1, y1) - point #1, (x2, y2) - point #2
 % output: psi
-
-% myu=[0;0;1];
-% k=2;
-% ri=[sin(thetai).*cos(phii);sin(thetai).*sin(phii);cos(thetai)];
-% rj=[sin(thetaj).*cos(phij);sin(thetaj).*sin(phij);cos(thetaj)];
-% rij=norm(ri-rj);
-%psi=1/(rij^2);
-%psi=1/(rij^2)/sqrt(Mises_Fisher(k,myu,ri))/sqrt(Mises_Fisher(k,myu,rj));
-%psi=1/(rij^2)/sqrt(Valee_Poussin(k,myu,ri))/sqrt(Valee_Poussin(k,myu,rj));
-% psi=1/(rij^2)/sqrt(PDF_Matrix(ri))/sqrt(PDF_Matrix(rj));
 
 if(x1 < W_2)
     f1 = 2;
@@ -136,28 +149,14 @@ else
     f2 = 1;
 end
 
-rij = sqrt((x2-x1).^2 + (y2-y1).^2);
+rij = sqrt((x2-x1)^2 + (y2-y1)^2);
 
 psi = 1/(rij^2)/sqrt(f1*f2);
 %psi = 1/(rij^2)/(f1*f2)^(1/4);
 %psi = -log(rij)/sqrt(f1*f2);
-
-E = 0.1;
-penetration_x = MaccellyBrace(abs(x1 - W_2) - W_2);
-penetration_y = MaccellyBrace(abs(y1 - H_2) - H_2);
-penetration2 = penetration_x^2 + penetration_y^2;
-penalty = -penetration2 * E * 1/2;
-
-psi = psi + penalty;
 end
 
-function value = MaccellyBrace(x)
-if(x >= 0)
-    value = x;
-else
-    value = 0;
-end
-end
+
 
 function handle = VisualizePoints(x, y, handle)
 % input:
