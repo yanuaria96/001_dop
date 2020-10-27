b8e521b842c75e52fdafe588a40ca5f78e129557
ndenis <denekra@yandex.ru>
2020-10-27 15:42:59 +0700

    Implemented different positions

--- rectangle/rect_pdf.m
+++ rectangle/rect_pdf.m
@@ -6,7 +6,6 @@ N = 100;
 c1_const = 0.000001;
 D_X = c1_const; %perturbation incriment
 D_Y = c1_const;
-D_T = .001*N^(-2);
 ETTA = 1;
 W = 2; % picture width
 H = 1; % picture height
@@ -19,58 +18,32 @@ vx = zeros(1,N); vy = zeros(1,N);
 handler = CreateRectangle(W, H);
 handler = VisualizePoints(x, y, handler);
 
-sumPsi_old = 1e100;
+% "Position" structure. Store an information about coordinates, velocities, sumPsi and maxVelocity
+position = {x: x, y: y, vx: vx, vy: vy, sumPsi: 1e100, maxVelocity: 0.0};
+% Vector of positions.  
+positions = [position];
+
 for iter = 1:10000
-    % Find potentials for each point
-    maxVelocity = 0;
-    sumPsi = 0;
-    for k = 1:N               
-        x_cur = x(k);
-        y_cur = y(k);
-        x_other = [x(1:k-1) x(k+1:end)];
-        y_other = [y(1:k-1) y(k+1:end)];
-%         x_other = x(1:k-1);
-%         y_other = y(1:k-1);
 
-        
-        [vx_cur, vy_cur, sumPsi_cur] = FindVelocity(x_cur, y_cur, x_other, y_other);
-        
-        vx(k) = vx_cur;
-        vy(k) = vy_cur;
-        
-        sumPsi = sumPsi + sumPsi_cur;        
-        v2 = vx_cur^2 + vy_cur^2;
-        maxVelocity = max([v2 maxVelocity]);
+    % Find vx, vy, sumPsi, maxVelosity for each position in positions vector
+    for j = 1:length(positions)
+        pos = positions(j); 
+    	[pos.vx, pos.vy, pos.sumPsi, pos.maxVelocity] = FindVelocities(pos.x, pos.y);   
     end
-   
-    
-    
-    
-    if sumPsi>sumPsi_old
-        D_T = D_T/1.1;
-        x = x_old;
-        y = y_old;
-    else
-        D_T = D_T*1.1;
-    
-%     if maxVelocity < 3e-4
-%         D_T = .04*N^(-2)*(maxVelocity/3e-4)^(-1/2);
-%     end
-    
 
-    % Move each object according to its velocity
-    x_old = x;
-    y_old = y;
-    sumPsi_old = sumPsi;
-    
-    x = x + vx*D_T;
-    y = y + vy*D_T;
-    end
-    
+    % Sort all positions by sumPsi
+    sort(positions.sumPsi);
+
+    % Best
+    pos = positions(1);
+    x = pos.x;
+    y = pos.y;
+    maxVelocity = pos.maxVelocity;
+    sumPsi = pos.sumPsi;
+
     if maxVelocity < 3e-10
         break;
     end
-
     
     if rem(iter,1) == 0
         handler = VisualizePoints(x, y, handler);
@@ -89,9 +62,50 @@ for iter = 1:10000
     if rem(iter,1) == 0
         SaveData(x, y);
     end
+
+
+    dt_array = [0.4 0.8 1.0 1.1 1.2];
+    for j = 1:length(dt_array)
+
+        pos = positions(1);
+        dt = dt_array(j);
+        pos.x = pos.x + pos.vx*dt;
+        pos.y = pos.y + pos.vy*dt;
+        
+        if j < length(positions)
+            positions(j) = pos;
+        else
+	    positions = [positions pos];
+        end
+    end
 end
 tt = toc(t);
 disp(tt);
 SaveData(x, y);
 
+end
+
+function [vx, vy, sumPsi, maxVelocity] = FindVelocities(x, y) 
+    % Find velocities (vx, and vy), sumPsi and max velocity for each point position (x,y)
+    % input: x, y - arrays of x and y coordinates
+    % output: vx, vy - arrays of velocities in x and y directions.
+    % sumPsi - sum psi
+    % maxVelocity - max velocity as vx*vx+vy*vy
+    maxVelocity = 0;
+    sumPsi = 0;
+    for k = 1:N               
+        x_cur = x(k);
+        y_cur = y(k);
+        x_other = [x(1:k-1) x(k+1:end)];
+        y_other = [y(1:k-1) y(k+1:end)];
+        
+        [vx_cur, vy_cur, sumPsi_cur] = FindVelocity(x_cur, y_cur, x_other, y_other);
+        
+        vx(k) = vx_cur;
+        vy(k) = vy_cur;
+        
+        sumPsi = sumPsi + sumPsi_cur;        
+        v2 = vx_cur^2 + vy_cur^2;
+        maxVelocity = max([v2 maxVelocity]);
+    end
 end
